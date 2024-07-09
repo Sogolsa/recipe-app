30fffb1a0140cdfa2fcfd5871d23c63fe82e75e4
Sogol <108494457+Sogolsa@users.noreply.github.com>
2024-07-09 15:45:22 -0600

    Linked the navbar to corresponding pages

--- src/recipes/urls.py
+++ src/recipes/urls.py
@@ -7,7 +7,7 @@ app_name = "recipes"
 
 # <pk> indicates the primary key of the object
 urlpatterns = [
-    path("", home),
+    path("", home, name="home"),
     path("recipes/", RecipeListView.as_view(), name="recipes"),
     path("recipes/<pk>", RecipeDetailView.as_view(), name="detail"),
 ]
