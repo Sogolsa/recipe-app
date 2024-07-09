7f547506a066047d266d196198e02268017dd174
Sogol <108494457+Sogolsa@users.noreply.github.com>
2024-07-09 10:14:39 -0600

    Specified the view (class-based)

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Recipe
 
 
 # Create your views here.
@@ -6,3 +8,15 @@ def home(request):
     """takes the request coming from the web application and returns
     the template available at recipes/home.html as a response."""
     return render(request, "recipes/recipes_home.html")
+
+
+class RecipeListView(ListView):
+    """Class based view"""
+
+    model = Recipe  # Specify model
+    template_name = "recipes/recipes_list.html"  # Specify template
+
+
+class RecipeDetailView(DetailView):
+    model = Recipe
+    template_name = "recipes/recipes_detail.html"
