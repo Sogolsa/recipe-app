86ac04dec391acd5a578c4708e9a7d3117a25b09
Sogol <108494457+Sogolsa@users.noreply.github.com>
2024-07-10 11:42:15 -0600

    Define User View

--- src/users/views.py
+++ src/users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import User
+
 
 # Create your views here.
+def profile(request):
+    user = User.objects.first()
+    context = {"user": user}
+    return render(request, "users/user_profile.html", context)
