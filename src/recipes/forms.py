178c138a0fa0ec879c5e01676b1219ecc3730f29
Sogol <108494457+Sogolsa@users.noreply.github.com>
2024-07-19 07:03:23 -0600

    Created form fields to search recipes

---
+++ src/recipes/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+CHART__CHOICES = (  # Specify choices as a tuple
+    ("#1", "Bar chart"),  # when user selects "Bar chart", it is stored as "#1"
+    ("#2", "Pie chart"),
+    ("#3", "Line chart"),
+)
+
+
+# Define Class based form imported from Django forms
+class RecipesSearchForm(forms.Form):
+    recipe_name = forms.CharField(max_length=120)
+    chart_type = forms.ChoiceField(choices=CHART__CHOICES)
