6d4e277fce87562c0765a2e1afbb3a48f1b19baf
Sogol <108494457+Sogolsa@users.noreply.github.com>
2024-08-09 13:21:48 -0600

    Updated allowed hosts

--- recipe_project/settings/production.py
+++ recipe_project/settings/production.py
@@ -1,7 +1,7 @@
 from .base import *
 from decouple import config
 import dj_database_url
-import os
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("SECRET_KEY")
@@ -9,16 +9,12 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "recipe-share-ofmj.onrender.com"]
 
 # Database
 
 DATABASES = {
-    "default": dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default=config("DATABASE_URL"),
-        conn_max_age=500,
-    )
+    "default": dj_database_url.config(default=config("DATABASE_URL"), conn_max_age=500),
 }
 
 STATICFILES_DIRS = [
