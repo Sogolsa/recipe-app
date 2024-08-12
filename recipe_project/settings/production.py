dbc4985e5dbb92c9f56b0e115a6b1414b2c5f5a5
Sogol <108494457+Sogolsa@users.noreply.github.com>
2024-08-12 15:24:31 -0600

    Updated the recipe list templatebecause of server error

--- recipe_project/settings/production.py
+++ recipe_project/settings/production.py
@@ -34,13 +34,35 @@ AWS_S3_CUSTOM_DOMAIN = "%s.s3.amazonaws.com" % AWS_STORAGE_BUCKET_NAME
 # Don't overwrite files with same name
 AWS_S3_FILE_OVERWRITE = False
 
-STORAGES = {
-    # CSS and JS file
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+# STORAGES = {
+#     # CSS and JS file
+#     "staticfiles": {
+#         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+#     },
+#     # Media files management
+#     "default": {
+#         "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+#     },
+# }
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+# Logging configuration
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "file": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "filename": os.path.join(BASE_DIR, "django_debug.log"),
+        },
     },
-    # Media files management
-    "default": {
-        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "DEBUG",
+            "propagate": True,
+        },
     },
 }
